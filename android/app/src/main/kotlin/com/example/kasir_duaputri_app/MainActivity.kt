c1ee61a6689b6f561f3033edd46da5f80445a966
Arief Setyo Nugroho <49858542+ASNProject@users.noreply.github.com>
2023-04-04 23:22:19 +0700

    base code

---
+++ android/app/src/main/kotlin/com/example/kasir_duaputri_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kasir_duaputri_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/presentation/dashboard_screen.dart
@@ -0,0 +1,36 @@
+// Copyright 2023 ariefsetyonugroho
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class DashboardScreen extends StatefulWidget {
+  const DashboardScreen({super.key});
+
+  @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: ElevatedButton(
+            onPressed: () => context.go('/transaksi'),
+            child: Text('Pindah ke transaksi')),
+      ),
+    );
+  }
+}
